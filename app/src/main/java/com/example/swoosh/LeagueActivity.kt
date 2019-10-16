f655600351dfe06cb9b3660c99939a2828425f64
Regis <reshimwe2025@gmailcom>
2019-10-16 12:38:30 +0200

    third screen

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,28 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+
+        //you either use event listener on ID or function on element attributes
+
+//        leagueNextBtn.setOnClickListener {
+//            val skillActivity = Intent(this, SkillActivity::class.java)
+//            startActivity(skillActivity)
+//        }
+    }
+
+    fun leagueNextClicked(view: View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
