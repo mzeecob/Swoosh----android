44ab546c0fae342f33bb0a0ac6542fd518436976
Regis <reshimwe2025@gmailcom>
2019-10-16 14:34:26 +0200

    connecting second and third screen together, using intent and pass over some data

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+
+    var selectedleague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +25,41 @@ class LeagueActivity : BaseActivity() {
 //        }
     }
 
-    fun leagueNextClicked(view: View){
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+
+
+    fun onMensClicked(view: View){
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedleague = "mens"
+
     }
+
+    fun onWomensClicked(view: View){
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedleague = "womens"
+
+    }
+
+    fun onCoedClicked(view: View){
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedleague = "co-ed"
+
+    }
+
+    fun leagueNextClicked(view: View){
+
+        if(selectedleague != ""){
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedleague)
+            startActivity(skillActivity)
+        }else {
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
