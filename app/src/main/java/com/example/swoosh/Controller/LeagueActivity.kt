5e1d4eea8adfa0a76eebac8b4176a6cb655f6af6
Regis <reshimwe2025@gmailcom>
2019-10-16 16:42:32 +0200

    done app

--- app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
@@ -15,6 +15,13 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
@@ -26,6 +33,14 @@ class LeagueActivity : BaseActivity() {
 //            val skillActivity = Intent(this, SkillActivity::class.java)
 //            startActivity(skillActivity)
 //        }
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        }
     }
 
 
